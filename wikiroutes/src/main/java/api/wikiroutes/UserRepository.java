1004010cecb8f5a1fb6adf8920d2c5f328d42e62
José Ignacio Escribano <jiep12345@hotmail.com>
2015-04-27 18:28:42 +0200

    Añadido UserRepository
    Añadidas consultas de buscar por id, buscar por userName y buscar por
    ApiKey

---
+++ wikiroutes/src/main/java/api/wikiroutes/UserRepository.java
@@ -0,0 +1,17 @@
+package api.wikiroutes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	// Busca usuarios por id
+	User findById(Long id);
+	
+	// Busca usuarios por userName
+	User findByUserName(String username);
+	
+	// Busca usuarios por apiKey
+	User findByApiKey(String apiKey);
+	
+
+}
