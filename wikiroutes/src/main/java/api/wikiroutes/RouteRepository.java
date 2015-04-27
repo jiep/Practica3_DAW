95a946c975e517c18c1cb2ca0c9422bc6f2822e5
José Ignacio Escribano <jiep12345@hotmail.com>
2015-04-27 20:09:59 +0200

    Añadidos más repositorios

---
+++ wikiroutes/src/main/java/api/wikiroutes/RouteRepository.java
@@ -0,0 +1,9 @@
+package api.wikiroutes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RouteRepository extends JpaRepository<Route, Long> {
+
+
+}
+
---
+++ wikiroutes/src/main/java/api/wikiroutes/TypeRouteRepository.java
@@ -0,0 +1,7 @@
+package api.wikiroutes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TypeRouteRepository extends JpaRepository<TypeRoute, Long> {
+
+}
