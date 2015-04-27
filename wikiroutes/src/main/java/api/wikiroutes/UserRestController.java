31738871a0ff4f30404049557fd1797d4bf29b3f
José Ignacio Escribano <jiep12345@hotmail.com>
2015-04-27 18:26:58 +0200

    Añadido ID 1 de la API REST
    #4

--- wikiroutes/src/main/java/api/wikiroutes/UserRestController.java
+++ wikiroutes/src/main/java/api/wikiroutes/UserRestController.java
@@ -2,16 +2,21 @@ package api.wikiroutes;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UserRestController {
+	
+	@Autowired
+	UserRepository users;
 
 	@RequestMapping
 	public List<User> getUsers() {
-		return null;
+		
+		return users.findAll();
 	}
 
 }
