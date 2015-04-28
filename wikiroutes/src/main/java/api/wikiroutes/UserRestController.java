8ad20bae8195450823f4718ee66506da13655167
José Ignacio Escribano <jiep12345@hotmail.com>
2015-04-28 17:32:20 +0200

    Añadido ID 3 de la API REST
    #3 #4

--- wikiroutes/src/main/java/api/wikiroutes/UserRestController.java
+++ wikiroutes/src/main/java/api/wikiroutes/UserRestController.java
@@ -23,20 +23,19 @@ public class UserRestController {
 
 	@RequestMapping
 	public List<User> getUsers() {
-		
+
 		return users.findAll();
 	}
-	
-	/*@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<User> addUser(@RequestBody User user) {
-		user.setRoutes(new ArrayList<Route>());
-		user.setComments(new ArrayList<Comment>());
-		user.setPass(HashPassword.generateHashPassword(user.getPass()));
-		user.setPermission(false);
-		user.setFriendships(new ArrayList<Friendship>());
-		User u = users.saveAndFlush(user);
-		return new ResponseEntity<>(u, HttpStatus.CREATED);
-	}*/
+
+	/*
+	 * @RequestMapping(method = RequestMethod.POST) public ResponseEntity<User>
+	 * addUser(@RequestBody User user) { user.setRoutes(new ArrayList<Route>());
+	 * user.setComments(new ArrayList<Comment>());
+	 * user.setPass(HashPassword.generateHashPassword(user.getPass()));
+	 * user.setPermission(false); user.setFriendships(new
+	 * ArrayList<Friendship>()); User u = users.saveAndFlush(user); return new
+	 * ResponseEntity<>(u, HttpStatus.CREATED); }
+	 */
 
 	@RequestMapping("/{id}")
 	public User getUserById(@PathVariable Long id) {
@@ -47,33 +46,20 @@ public class UserRestController {
 
 		return users.findById(id);
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public void deleteUserById(@PathVariable Long id) {
-		
-		if(users.findById(id) != null){
+
+		if (users.findById(id) != null) {
 			users.delete(id);
-		}		
+		}
 	}
 
-	/*@RequestMapping("/{id}/routes")
+	@RequestMapping("/{id}/routes")
 	public List<Route> getRoutesById(@PathVariable Long id) {
-		
-
-		TypeRoute tr = new TypeRoute("Bici");
-
-		types.save(tr);
-
-		Route r = new Route("Ruta buena", "Una ruta digna de mención", users.findById(id), tr,
-				5, true);
-
-		routes.saveAndFlush(r);
-		
-		users.findOne(id).getRoutes().add(r);
 
 		return users.findOne(id).getRoutes();
-	}*/
-	
-	
+		
+	}
 
 }
