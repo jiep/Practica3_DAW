f98dd5490dc606c7b1766ca47cbb2283bf923682
German <german.alonso9@gmail.com>
2015-05-19 15:48:13 +0200

    intento imagenes 1

--- wikiroutes/src/main/resources/static/app/app.js
+++ wikiroutes/src/main/resources/static/app/app.js
@@ -1,6 +1,8 @@
 var app = angular.module('WikiroutesApp', [ 'ngMaterial', 'uiGmapgoogle-maps',
 		'ngMdIcons', 'ngRoute', 'ngResource', 'slick', 'ngMessages',
-		'wikiroutes.services' ]);
+		'wikiroutes.services', 'ngDroplet' ]);
+
+
 
 app.config(function($mdThemingProvider) {
 	$mdThemingProvider.theme('default').primaryPalette('blue-grey')
@@ -100,7 +102,7 @@ function NavCtrl($timeout, $q, $log) {
 };
 
 app.controller('MapCtrl', function($scope, $rootScope, Route, $location) {
-
+	
 	$scope.route = {};
 
 	$scope.map = {
@@ -354,4 +356,128 @@ app.controller("ProfileCtrl", function($rootScope, $scope) {
 	$scope.routes = $rootScope.user.routes;
 	console.log("Rutas root", $rootScope.user.routes);
 
+});
+
+
+
+app.controller('ImgCtrl', function IndexController($scope, $timeout) {
+
+        /**
+         * @property interface
+         * @type {Object}
+         */
+        $scope.interface = {};
+
+        /**
+         * @property uploadCount
+         * @type {Number}
+         */
+        $scope.uploadCount = 0;
+
+        /**
+         * @property success
+         * @type {Boolean}
+         */
+        $scope.success = false;
+
+        /**
+         * @property error
+         * @type {Boolean}
+         */
+        $scope.error = false;
+
+        // Listen for when the interface has been configured.
+        $scope.$on('$dropletReady', function whenDropletReady() {
+
+            $scope.interface.allowedExtensions(['png', 'jpg', 'bmp', 'gif', 'svg', 'torrent']);
+            $scope.interface.setRequestUrl('upload.html');
+            $scope.interface.defineHTTPSuccess([/2.{2}/]);
+            $scope.interface.useArray(false);
+
+        });
+
+        // Listen for when the files have been successfully uploaded.
+        $scope.$on('$dropletSuccess', function onDropletSuccess(event, response, files) {
+
+            $scope.uploadCount = files.length;
+            $scope.success     = true;
+            console.log(response, files);
+
+            $timeout(function timeout() {
+                $scope.success = false;
+            }, 5000);
+
+        });
+
+        // Listen for when the files have failed to upload.
+        $scope.$on('$dropletError', function onDropletError(event, response) {
+
+            $scope.error = true;
+            console.log(response);
+
+            $timeout(function timeout() {
+                $scope.error = false;
+            }, 5000);
+
+        });
+
+    }).directive('progressbar', function ProgressbarDirective() {
+
+        return {
+
+            /**
+             * @property restrict
+             * @type {String}
+             */
+            restrict: 'A',
+
+            /**
+             * @property scope
+             * @type {Object}
+             */
+            scope: {
+                model: '=ngModel'
+            },
+
+            /**
+             * @property ngModel
+             * @type {String}
+             */
+            require: 'ngModel',
+
+            /**
+             * @method link
+             * @param scope {Object}
+             * @param element {Object}
+             * @return {void}
+             */
+            link: function link(scope, element) {
+
+                var progressBar = new ProgressBar.Path(element[0], {
+                    strokeWidth: 2
+                });
+
+                scope.$watch('model', function() {
+
+                    progressBar.animate(scope.model / 100, {
+                        duration: 1000
+                    });
+
+                });
+
+                scope.$on('$dropletSuccess', function onSuccess() {
+                    progressBar.animate(0);
+                });
+
+                scope.$on('$dropletError', function onSuccess() {
+                    progressBar.animate(0);
+                });
+
+            }
+
+        }
+
 })
+
+
+
